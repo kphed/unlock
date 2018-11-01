d562a98769ef72c23dee2fe103d6dbd9c9594db5
julien <julien.genestoux@gmail.com>
2018-11-01 14:42:35 -0400

    setup for tests

---
+++ tests/helpers/setup.js
@@ -0,0 +1,17 @@
+const os = require('os')
+const path = require('path')
+const fs = require('fs-extra')
+const puppeteer = require('puppeteer')
+
+const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup')
+
+module.exports = async () => {
+
+
+  const browser = (global.BROWSER = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox'],
+  }))
+
+  await fs.outputFile(path.join(DIR, 'wsEndpoint'), browser.wsEndpoint())
+}
